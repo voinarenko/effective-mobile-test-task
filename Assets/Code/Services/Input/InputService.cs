b7343210d6fa02ecb413807c0438c1580a0680f4
Sergei Voinarenko <voynarenko@gmail.com>
2025-10-17 15:30:55 +0300

    add dispose method to input service for proper event unsubscription and cleanup

--- Assets/Code/Services/Input/InputService.cs
+++ Assets/Code/Services/Input/InputService.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine.InputSystem;
+﻿using Code.Data;
+using System;
+using UnityEngine.InputSystem;
 
 namespace Code.Services.Input
 {
-  public class InputService : IInputService
+  public class InputService : IInputService, IDisposable
   {
     private readonly PlayerInputActions _controls;
     private InputDevice _lastUsedDevice;
@@ -11,8 +13,18 @@ namespace Code.Services.Input
     {
       _controls = new PlayerInputActions();
       _controls.Enable();
+      _controls.UI.Cancel.performed += OnCancel;
     }
 
     public PlayerInputActions GetActions() => _controls;
+
+    public void Dispose()
+    {
+      _controls.UI.Cancel.performed -= OnCancel;
+      _controls.Disable();
+    }
+
+    private void OnCancel(InputAction.CallbackContext context) =>
+      Utils.Quit();
   }
 }
